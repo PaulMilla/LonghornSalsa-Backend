24cfefb3e5d23066fd8944314b07c8ba861e1341
Paul Milla <paul.milla1@utexas.edu>
2018-06-27 23:58:27 -0500

    Allow us to set an access token manually

--- server.js
+++ server.js
@@ -2,34 +2,61 @@ var express = require('express');
 var cors = require('cors');
 var facebook = require('fb');
 
-var FB = new facebook.Facebook({
-    appId: process.env.APP_ID,
-    appSecret: process.env.APP_SECRET});
-
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 
-// Generate an access token
-let options = {
-    client_id: process.env.APP_ID,
-    client_secret: process.env.APP_SECRET,
-    grant_type: 'client_credentials'};
-FB.api('oauth/access_token', options, function (res) {
-    if(!res || res.error) {
-        console.log(!res ? 'error occurred' : res.error);
-        return;
-    }
-    FB.setAccessToken(res.access_token);
-});
-
-
 var app = express();
 app.use(cors());
 
+var FB = facebook.default;
+FB.options({ version: 'v3.0' });
+
+/* To generate an *extended* access token manually:
+ * go to facebook graph explorer
+ * - https://developers.facebook.com/tools/explorer/
+ * - Click "Get Token" dropdown on righthand side
+ * - Under "Page Access Tokens" select "Longhorn Salsa"
+ * - Press the blue & white "i" circle button on the Access Token bar
+ * - "Open in Access Token Tool"
+ * - It should've taken you to the Access Token Debugger
+ * - https://developers.facebook.com/tools/debug/accesstoken/
+ * - On the bottom of the Access Token Info box should be a button "Extend Access Token"
+ * - Pressing it will extend the access token from 1 hr to 2 months
+ * - Update the ACCESS_TOKEN environment variable to reflect the changes
+ */
+if (process.env.ACCESS_TOKEN) {
+    console.log('Using manually provided access token.')
+    FB.setAccessToken(process.env.ACCESS_TOKEN);
+}
+else {
+    // Generate an access token
+    console.log('Using client_id: '+process.env.APP_ID);
+    console.log('Using client_secret: '+process.env.APP_SECRET);
+
+    FB.api(
+    'oauth/access_token',
+    {
+        client_id: process.env.APP_ID,
+        client_secret: process.env.APP_SECRET,
+        grant_type: 'client_credentials'
+    },
+    function (res) {
+        if(!res || res.error) {
+            console.log(!res ? 'error occurred' : res.error);
+            return;
+        }
+        console.log('Successfully generated an app access token: '+res.access_token);
+        FB.setAccessToken(res.access_token);
+    });
+}
+
+console.log('Using access token: '+FB.options('accessToken'));
+
 //////////// Routes /////////////////
+// LonghornSalsa PageID = 466676286853256
 app.get('/', function(req, res) {
-    res.send("Hello World! <a href='/events/1700480319978856'>ClickMe!</a>");
+    res.send("Hello World! <a href='/events/200333530611671'>ClickMe!</a>");
 });
 
 app.get('/events', function(req, res) {
@@ -83,3 +110,5 @@ app.get('/:eventId/picture', function(req, res) {
 app.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port);
 });
+
+
